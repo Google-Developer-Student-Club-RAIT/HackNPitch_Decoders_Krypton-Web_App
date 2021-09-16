73efa12c670b3a79e6242a3eced2baa0bf0291d1
Narottam04 <coutkunal@gmail.com>
2021-09-16 09:26:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Main from './Pages/main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, quo.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Nav } from '../../Components/Navbar'
+
+function Main() {
+    return (
+        <div>
+            <Nav />
+        </div>
+    )
+}
+
+export default Main
