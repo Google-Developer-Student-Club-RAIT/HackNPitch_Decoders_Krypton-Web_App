1ee5630aed06507a37d59ac12a18003d5b9b3155
Zaid Khan <israrahmed@Zaids-MacBook-Air.local>
2021-09-20 18:15:57 +0530

    Styled Login Signup  and Dashboard

---
+++ src/Components/News.js
@@ -0,0 +1,12 @@
+import React, {useContext} from 'react'
+ 
+ const News = () => {
+     return (
+         <>
+              <h4 className="my-4 mx-4 text-4xl">News</h4>
+         </>
+     )
+ }
+ 
+ export default News
+ 
--- src/Components/TransactionList.js
+++ src/Components/TransactionList.js
@@ -6,11 +6,12 @@ export const TransactionList = () => {
 
   const {transactions}= useContext(Globalcontext);
     return (
-        <div>
-            <h3>History</h3>
-      <ul id="list" className="list">
+        <>
+             <h4 className="my-4 mx-4 text-4xl">History</h4>
+             <ul id="list" className="list">
         {transactions.map(transaction => ( <Transaction key={transaction.id} transaction={transaction}/> ))}
       </ul>
-        </div>
+             <button className="text-lg float-right mt-52 mr-4 hover:text-yellow-300">View More..</button>
+        </>
     )
 }
