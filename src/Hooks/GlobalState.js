a3a7bfd5b5153a57f93c114ddf9f6bfc87496905
krish10k <krushna.dahake.01@gmail.com>
2021-09-24 13:14:24 +0530

    context fixed

--- src/Hooks/GlobalState.js
+++ src/Hooks/GlobalState.js
@@ -1,35 +1,52 @@
-import React, {createContext, useContext, useReducer} from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
-const initialState = {
-    transactions: []
+const savedTransaction = localStorage.getItem('transaction');
+// let initialState = {
+//     transactions: []
+// }
+let initialState
+// if stored
+if (savedTransaction) {
+  initialState = {
+    transactions: JSON.parse(savedTransaction)
+  }
+} else {
+  initialState = { transactions: [] }
 }
-
+console.log("istate", initialState)
 //Create Context
 export const Globalcontext = createContext(initialState);
 
 //Provider Component
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
-// Actions
+  // Actions
 
-function deleteTransaction(id){
-  dispatch({
-    type: 'DELETE_TRANSACTION',
-    payload: id
-  });
-}
+  function deleteTransaction(id) {
 
-function addTransaction(transaction){
-  dispatch({
-    type: 'ADD_TRANSACTION',
-    payload: transaction
-  });
-}
+    let list = localStorage.getItem('transaction');
+    if (list) {
+      let newList = JSON.parse(list).filter((item) => item.id !== id)
+      localStorage.setItem("transaction", JSON.stringify(newList));
+    }
 
-  return(
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
+  }
+
+  return (
     <Globalcontext.Provider value={{
       transactions: state.transactions,
-      deleteTransaction, 
+      deleteTransaction,
       addTransaction
     }}>
       {children}
