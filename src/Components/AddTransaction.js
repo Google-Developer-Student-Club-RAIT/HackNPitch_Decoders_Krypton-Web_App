0554b9b89d19b1d46626a59205b8a3414635c361
krish10k <krushna.dahake.01@gmail.com>
2021-09-24 10:41:59 +0530

    transaction storage

--- src/Components/AddTransaction.js
+++ src/Components/AddTransaction.js
@@ -1,27 +1,45 @@
-import React, {useState, useContext} from 'react'
-import {Globalcontext} from '../Hooks/GlobalState'
+import React, { useState, useContext } from 'react'
+import { Globalcontext } from '../Hooks/GlobalState'
 
 
 export const AddTransaction = () => {
-    const [text, setText] = useState('');
-    const [amount, setAmount] = useState('');
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState('');
 
-    const {addTransaction} = useContext(Globalcontext);     
-
-    const submitHandler = e => {
-      e.preventDefault();
-
-      const newTransaction = {
-        id: Math.floor(Math.random() * 10000000),
-        text,
-        amount: +amount
-      }
-      addTransaction(newTransaction);
+  const [transaction, setTransaction] = useState(() => {
+    // get from localstorage
+    const savedTransaction = localStorage.getItem('transaction');
+    // if stored
+    if (savedTransaction) {
+      return JSON.parse(savedTransaction);
+    } else {
+      return [];
     }
+  });
 
-    return (
-        <div>
-            <h3 className="my-6 mx-4 text-3xl">Add new transaction</h3>
+  React.useEffect(() => {
+    localStorage.setItem("transaction", JSON.stringify(transaction));
+    console.log("transactions-", transaction);
+  }, [transaction]);
+
+  const { addTransaction } = useContext(Globalcontext);
+  const submitHandler = e => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 10000000),
+      text,
+      amount: +amount
+    }
+    addTransaction(newTransaction);
+    setTransaction([
+      ...transaction,
+      newTransaction
+    ])
+  }
+
+  return (
+    <div>
+      <h3 className="my-6 mx-4 text-3xl">Add new transaction</h3>
       <form id="form">
         <div className="form-control">
           {/* <label htmlFor="text">Text</label> */}
@@ -32,10 +50,10 @@ export const AddTransaction = () => {
             >Amount <br />
             (negative - expense, positive - income)</label
           > */}
-          <input type="number" value={amount} onChange={(e)=>setAmount(e.target.value)} id="amount" placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" placeholder="Enter amount..." />
         </div>
         <button onClick={submitHandler} className="btn">Add transaction</button>
       </form>
-        </div>
-    )
+    </div>
+  )
 }
